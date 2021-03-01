007fa85a2c1d7f7fcfc5ebcaca61f92befae6c9d
LuisBoto <luisboto@hotmail.es>
2021-03-01 15:48:53 +0100

    Added R script execution functionality

--- src/logic/scripter/RScriptRunner.java
+++ src/logic/scripter/RScriptRunner.java
@@ -7,16 +7,20 @@ import javax.script.ScriptException;
 public class RScriptRunner {
 
 	public static void runRScript(String script) {
+		System.out.println("Initializing R parsing engine...");
 		ScriptEngineManager factory = new ScriptEngineManager();
 		// Create a Renjin engine
 		ScriptEngine engine = factory.getEngineByName("Renjin");
 		String[] lines = script.split("\n");
+		System.out.println("Executing R script");
 		try {
 			for (int i = 0; i < lines.length; i++)
 				engine.eval(lines[i]);
 		} catch (ScriptException e) {
 			e.printStackTrace();
 		}
+
+		System.out.println("R execution finished");
 	}
 
 }
--- src/logic/scripter/Scripter.java
+++ src/logic/scripter/Scripter.java
@@ -23,8 +23,8 @@ public class Scripter {
 	}
 
 	public static String createScript(String workDirectory, List<Metric> metrics, List<GraphCommand> graphs) {
-		// TODO: Figure out directory to put in here
-		StringBuilder script = new StringBuilder("setwd('" + workDirectory + "')\n");
+		// TODO: Work directory cannot be changed from Renjin
+		StringBuilder script = new StringBuilder("");//"setwd('" + workDirectory + "')\n");
 
 		// First declare all metrics that will be used
 		for (int i = 0; i < metrics.size(); i++) {
