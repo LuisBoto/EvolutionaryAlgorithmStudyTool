0b900ca5ee1fc3949869f1b0a28b15052c273e94
luisboto <luisboto@hotmail.es>
2021-03-10 10:39:43 +0100

    Added plot graph command

---
+++ src/logic/scripter/graphs/Plot.java
@@ -0,0 +1,32 @@
+package logic.scripter.graphs;
+
+import java.util.List;
+
+import logic.scripter.Metric;
+
+public class Plot extends GraphCommand {
+
+	public Plot(String pdfName, List<Metric> metrics) {
+		super(pdfName, metrics);
+	}
+
+	@Override
+	public String execute() {
+		// plot method call, should return
+		// plot(value1, value2, parameters...)
+		StringBuilder res; 
+		if (this.getMetrics().size()!=2) //TODO: Plot can only use two metrics...
+			res = new StringBuilder("#plot(");
+		else
+			res = new StringBuilder("plot(");
+		// Concatenation of data parameters
+		int metricSize = this.getMetrics().size();
+		for (int i = 0; i < metricSize-1; i++) {
+			res.append(this.getMetrics().get(i).getName().concat(","));
+		}
+		res.append(this.getMetrics().get(metricSize-1).getName()).append(")");
+		
+		return res.toString();
+	}
+
+}
