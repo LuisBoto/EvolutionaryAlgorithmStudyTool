e9cf8c90f503c9ae004c44b945efd3c0062fe8e8
Luis Boto <luisboto@hotmail.es>
2021-03-21 12:15:29 +0100

    Added normality test method

--- src/logic/scripter/RScriptRunner.java
+++ src/logic/scripter/RScriptRunner.java
@@ -5,8 +5,25 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 import org.renjin.eval.EvalException;
+import org.renjin.sexp.ListVector;
 
 public class RScriptRunner {
+
+	public static void main(String[] args) throws EvalException, ScriptException {
+		// Main method to test individual scripts
+
+		ScriptEngineManager factory = new ScriptEngineManager();
+		ScriptEngine engine = factory.getEngineByName("Renjin");
+
+		engine.eval("require(tidyverse)");
+		engine.eval("require(xlsx)");
+		engine.eval("require(reshape)");
+		engine.eval("require(PMCMRplus)");
+		engine.eval("data<-c(1,2,3,4,5,6,7,8,9)");
+		ListVector res = (ListVector) engine.eval("shapiro.test(data)");
+		System.out.println(res);
+		System.out.println(res.getElementAsDouble(1)); // p value
+	}
 
 	public static void runRScript(String script) throws ScriptException, EvalException {
 		System.out.println("Initializing R parsing engine...");
@@ -19,6 +36,18 @@ public class RScriptRunner {
 			engine.eval(lines[i]);
 		}
 		System.out.println("R execution finished");
+	}
+
+	public static double normalityTest(Metric metric) throws ScriptException {
+		ScriptEngineManager factory = new ScriptEngineManager();
+		ScriptEngine engine = factory.getEngineByName("Renjin");
+		engine.eval("require(tidyverse)");
+		engine.eval("require(xlsx)");
+		engine.eval("require(reshape)");
+		engine.eval("require(PMCMRplus)");
+		engine.eval(metric.toString());
+		ListVector res = (ListVector) engine.eval("shapiro.test(" + metric.getName() + ")");
+		return res.getElementAsDouble(1);
 	}
 
 }
