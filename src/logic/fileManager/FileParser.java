ddf357ff143628b5a5197c2685cdfba3ed500adc
luisboto <luisboto@hotmail.es>
2021-05-02 13:56:38 +0200

    unified R engine, fixed bug on empty metric parsing

--- src/logic/fileManager/FileParser.java
+++ src/logic/fileManager/FileParser.java
@@ -21,14 +21,13 @@ public class FileParser {
 		String line = bf.readLine();
 		// First line is composed of metric names
 		for (int i = 0; i < line.split(";").length; i++) {
-			parsedMetrics.add(new Metric(line.split(";")[i]+id+i));
+			parsedMetrics.add(new Metric(line.split(";")[i] + id + i));
 		}
 		// Now onto parsing values
 		while ((line = bf.readLine()) != null) {
 			String[] pieces = line.split(";");
 			for (int i = 0; i < pieces.length; i++) {
-				if (!pieces[i].equals(""))
-					parsedMetrics.get(i).addValue(pieces[i]);
+				parsedMetrics.get(i).addValue(pieces[i]);
 			}
 		}
 		validateParsedMetrics();
@@ -40,15 +39,24 @@ public class FileParser {
 	private static void validateParsedMetrics() {
 		for (int i = 0; i < parsedMetrics.size(); i++) {
 			Metric metric = parsedMetrics.get(i);
+			if (metric.getSize() <= 0) {
+				i = removeMetric(i);
+				continue;
+			}
+
 			for (int j = 0; j < metric.getValues().size(); j++) {
-				if (!isNumeric(metric.getValues().get(j))) {
-					System.out.println("Removing " + parsedMetrics.get(i).getName());
-					parsedMetrics.remove(i);
-					i -= 1;
+				if (!isNumeric(metric.getValues().get(j)) || metric.getValues().get(j).equals("")) {
+					i = removeMetric(i);
 					break;
 				}
 			}
 		}
+	}
+
+	private static int removeMetric(int index) {
+		System.out.println("Removing " + parsedMetrics.get(index).getName());
+		parsedMetrics.remove(index);
+		return index - 1;
 	}
 
 	public static boolean isNumeric(String value) {
